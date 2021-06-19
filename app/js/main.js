3d145be910a0a78c83c65547bb9f52c0dc245fe4
Evgenii Dianov <dianov.js@gmail.com>
2021-06-19 17:44:07 +0300

    Finish

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,23 @@
-const a = 5;
+// !function(){
+//     var $winPopup = document.querySelector('.win-popup');
+//     var $wheel = document.querySelector('.landing-17__wheel-wrap--img');
+//     var $wheelBtn = document.querySelector('.landing-17__wheel-wrap--btn');
 
-console.log(a);
+//     $wheelBtn.addEventListener('click', function(e) {
+//         $wheel.classList.add('active');
+//         setTimeout(function() {
+//             $winPopup.classList.add('active');
+//         }, 3500);
+//     });
+// }();
+
+const popup = document.querySelector(".popup")
+const wheel = document.querySelector(".wheel")
+const wheelBtn = document.querySelector(".wheel__btn_wrap")
+
+wheelBtn.addEventListener("click", () => {
+  wheel.classList.add("active")
+  setTimeout(() => {
+    popup.classList.add("active")
+  }, 3500)
+})
